36125384a5804827dfc53b9eb8cdc6f0fb6cf280
Daniel García Campoy <daniel.garcia.campoy@gmail.com>
2022-10-07 09:22:45 +0200

    adivinaNumero

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,32 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  num:number;
+  mayorMenor = "...";
+
   constructor() {}
+
+  numAleatorio(a, b) {
+    return Math.round(Math.random() * (b - a) + parseInt(a, 10));
+  }
+  
+  numSecreto: number = this.numAleatorio(0,100);
+
+  compruebaNumero(){
+    if(this.num)
+    {
+      if(this.numSecreto < this.num)
+      {
+        this.mayorMenor = 'mayor que';
+      }
+      else if(this.numSecreto > this.num)
+      {
+        this.mayorMenor = 'menor que';
+      }
+      else{
+        this.mayorMenor = '';
+      }
+    }
+  }
 
 }
